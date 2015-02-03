729a3196b2b9232bfdb5ac9053da57c4123c6872
crackerhead <nbower@mac.com>
2015-02-02 19:41:39 -0800

    added admin

--- js/feed.js
+++ js/feed.js
@@ -37,7 +37,29 @@ var BlogsView =  Parse.View.extend({
     }
 });
 
+$('.form-signin').on('submit', function(e) {
 
+    // Prevent Default Submit Event
+    e.preventDefault();
+
+    // Get data from the form and put them into variables
+    var data = $(this).serializeArray(),
+        username = data[0].value,
+        password = data[1].value;
+
+    // Call Parse Login function with those variables
+    Parse.User.logIn(username, password, {
+        // If the username and password matches
+        success: function(user) {
+            alert('Welcome!');
+        },
+        // If there is an error
+        error: function(user, error) {
+            console.log(error);
+        }
+    });
+
+});
 
 
 
